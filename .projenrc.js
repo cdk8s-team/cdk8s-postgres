09850ac806010862457490060136c8440c436450
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2024-01-22 13:53:24 -0500

    Update .projenrc.js
    Signed-off-by: Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,6 @@ const { Cdk8sTeamJsiiProject } = require('@cdk8s/projen-common');
 const project = new Cdk8sTeamJsiiProject({
   defaultReleaseBranch: 'main',
   minNodeVersion: '14.18.0',
-  workflowNodeVersion: '16.20.0',
   release: false,
   name: 'cdk8s-postgres',
   peerDeps: [
